9bb1ca97f438d44e259c98cf1ed8aff1b767534f
dhysong <dhysong@gmail.com>
2017-09-29 09:42:01 -0400

    Added homepage logo

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">SpellWith.Me</h1>
+          <img src={process.env.PUBLIC_URL + '/images/SpellWithMe_logo_md.jpg'}  className="App-logo" alt="SpellWith.Me" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
