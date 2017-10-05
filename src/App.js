21b7d8c85b2b65412232d87d04f8a0e08d297de6
dhysong <dhysong@gmail.com>
2017-10-05 16:46:48 -0400

    Dunno

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ class App extends Component {
 		return (
 			<Provider store={store}>
 				<div className="App">
-          <header className="App-header">
-            <img src={process.env.PUBLIC_URL + '/images/SpellWithMe_logo_md.jpg'}  className="App-logo" alt="SpellWith.Me" />
-          </header>
-          <div className="App-title">SpellWith.Me</div>
+					<header className="App-header">
+						<img src={process.env.PUBLIC_URL + '/images/SpellWithMe_logo_md.jpg'}  className="App-logo" alt="SpellWith.Me" />
+					</header>
+					<div className="App-title">SpellWith.Me</div>
 					<GoogleLogin />        
 				</div> 
 
